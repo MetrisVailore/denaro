2980322c92735555d4ce3a7fbf5ae3cc67ae994e
MetrisVailore <122095030+MetrisVailore@users.noreply.github.com>
2025-03-13 19:19:11 +0100

    Update constants.py

--- denaro/constants.py
+++ denaro/constants.py
@@ -4,6 +4,7 @@ ENDIAN = 'little'
 CURVE = curve.P256
 SMALLEST = 1000000
 MAX_SUPPLY = 30_062_005
+BLOCK_TIME = 30
 VERSION = 1
 MAX_BLOCK_SIZE_HEX = 4096 * 1024  # 4MB in HEX format, 2MB in raw bytes
 DEV_NODE = "https://"
