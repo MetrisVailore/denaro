e8e5e08932c210fe46ed3a0a23ea94c26f014714
MetrisVailore <122095030+MetrisVailore@users.noreply.github.com>
2025-03-13 19:21:16 +0100

    Update constants.py

--- denaro/constants.py
+++ denaro/constants.py
@@ -1,4 +1,5 @@
 from fastecdsa import curve
+from decimal import Decimal
 
 ENDIAN = 'little'
 CURVE = curve.P256
@@ -6,7 +7,7 @@ SMALLEST = 1000000
 MAX_SUPPLY = 30_062_005
 BLOCK_TIME = 30
 BLOCKS_DIFF_CHANGE = Decimal(120)
-START_DIFF
+START_DIFF = Decimal('3.0')
 VERSION = 1
 MAX_BLOCK_SIZE_HEX = 4096 * 1024  # 4MB in HEX format, 2MB in raw bytes
 DEV_NODE = "https://"
